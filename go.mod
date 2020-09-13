15062b7ae8fdce9014e675aa12ecfbc2610f0663
Prem Kumar <prmsrswt@gmail.com>
2020-09-13 19:13:53 +0530

    Setup boilerplate Gin server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/prmsrswt/edu-accounts
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"messsage": "pong"})
+	})
+
+	srv := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: router,
+
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %s\n", err)
+	}
+	log.Println("Server exiting")
+}
