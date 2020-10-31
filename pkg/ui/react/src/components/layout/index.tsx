6c5be50b912dba5cc85ff87ca7d3d8118d134724
Prem Kumar <prmsrswt@gmail.com>
2020-10-31 20:03:26 +0530

    Made the dashboard outer layout responsive

--- pkg/ui/react/src/components/layout/index.tsx
+++ pkg/ui/react/src/components/layout/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../../contexts/auth";
+import { User } from "../../interfaces";
 import logoImg from "../../assets/logo.png";
 import styles from "./style.module.css";
-import { Link, NavLink } from "react-router-dom";
+
+const UserInfo = ({ user }: { user: User }) => {
+  return (
+    <>
+      <img
+        className={styles.navPhoto}
+        src={
+          user?.photo ||
+          `https://ui-avatars.com/api/?name=${user.name}&size=196&background=465062&color=f1f1f1`
+        }
+        alt="user"
+      />
+      <h4 className={styles.navName}>{user.name}</h4>
+    </>
+  );
+};
 
 const Nav: React.FC = () => {
   const { user } = useAuth();
@@ -15,15 +32,7 @@ const Nav: React.FC = () => {
       <div className={styles.accountDiv}>
         {user && (
           <>
-            <img
-              className={styles.navPhoto}
-              src={
-                user?.photo ||
-                `https://ui-avatars.com/api/?name=${user.name}&size=196&background=465062&color=f1f1f1`
-              }
-              alt="user"
-            />
-            <h4 className={styles.navName}>{user.name}</h4>
+            <UserInfo user={user} />
             <div className={styles.dropdown}>
               <Link to="/logout">Logout</Link>
             </div>
@@ -40,7 +49,12 @@ export const Layout: React.FC = ({ children }) => {
     <div className={styles.container}>
       <Nav />
       <section className={styles.main}>
+        <input type="checkbox" className={styles.exInput} id="expand" />
+        <label className={styles.expand} htmlFor="expand">
+          X
+        </label>
         <nav className={styles.sidebar}>
+          <div className={styles.smolAccountDiv}>{user && <UserInfo user={user} />}</div>
           <NavLink exact activeClassName={styles.active} to="/profile">
             Profile
           </NavLink>
@@ -61,6 +75,10 @@ export const Layout: React.FC = ({ children }) => {
               </NavLink>
             </>
           )}
+          <div className={styles.mobileUser}>
+            <hr />
+            <Link to="/logout">Logout</Link>
+          </div>
         </nav>
         <div className={styles.content}>{children}</div>
       </section>
