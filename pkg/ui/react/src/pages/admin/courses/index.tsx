c49fdc3980c735792187f94a75751b7967af0474
Prem Kumar <prmsrswt@gmail.com>
2020-11-01 23:29:03 +0530

    Allow adding new course from UI

--- pkg/ui/react/src/pages/admin/courses/index.tsx
+++ pkg/ui/react/src/pages/admin/courses/index.tsx
@@ -1,9 +1,100 @@
-import React from "react";
-import useSWR from "swr";
+import React, { useState } from "react";
+import useSWR, { mutate } from "swr";
+import { useForm } from "react-hook-form";
+
 import Layout from "../../../components/layout";
+import Button from "../../../components/button";
+import { Label, Text } from "../../../components/form";
+import Modal from "../../../components/modal";
 
 import { Course } from "../../../interfaces";
+import useSWRPost from "../../../hooks/useSWRPost";
+import { ReactComponent as Loader } from "../../../assets/three-dots.svg";
+
 import styles from "./style.module.css";
+
+interface Payload {
+  name: string;
+  code: string;
+  semesters: string;
+}
+
+const AddCourse = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { register, handleSubmit } = useForm<Payload>();
+  const [createCourse, { isValidating }] = useSWRPost<string>("/api/course", {
+    onSuccess: (res) => {
+      if (res.status === "success") {
+        mutate("/api/courses");
+        setIsOpen(false);
+      } else console.log(res);
+    },
+    onError: console.log,
+  });
+
+  const onSubmit = (values: Payload) => {
+    const p = {
+      ...values,
+      semesters: Number(values.semesters),
+    };
+    createCourse(JSON.stringify(p));
+  };
+  return (
+    <>
+      <Button
+        type="button"
+        style={{ background: "var(--color-brand)", width: "max-content" }}
+        onClick={() => setIsOpen(true)}
+      >
+        Add New Course
+      </Button>
+      <Modal isOpen={isOpen} toggleModal={setIsOpen}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.addForm}>
+          <h2>Create New Course</h2>
+          <Label value="Name">
+            <Text
+              name="name"
+              placeholder="Name of the Course"
+              inpRef={register({
+                required: true,
+              })}
+            />
+          </Label>
+          <Label value="Code">
+            <Text
+              name="code"
+              placeholder="Short code; ex. IMT"
+              inpRef={register({
+                required: true,
+              })}
+            />
+          </Label>
+          <Label value="Semesters">
+            <Text
+              name="semesters"
+              type="number"
+              placeholder="No. of semesters"
+              inpRef={register({
+                required: true,
+              })}
+            />
+          </Label>
+          <Button
+            type="button"
+            onClick={() => setIsOpen(false)}
+            style={{ background: "var(--color-danger)" }}
+            disabled={isValidating}
+          >
+            Cancel
+          </Button>
+          <Button style={{ background: "var(--color-success)" }} disabled={isValidating}>
+            {isValidating ? <Loader style={{ height: "1rem" }} /> : `Submit`}
+          </Button>
+        </form>
+      </Modal>
+    </>
+  );
+};
 
 const CourseRow: React.FC<{ course: Course }> = ({ course }) => {
   const { id, name, code, semesters } = course;
@@ -18,9 +109,7 @@ const CourseRow: React.FC<{ course: Course }> = ({ course }) => {
 };
 
 const Courses = () => {
-  const { data, isValidating } = useSWR<{ status: string; courses: Course[] }>(`/api/courses`);
-
-  if (isValidating) return <h2>Loading...</h2>;
+  const { data } = useSWR<{ status: string; courses: Course[] }>(`/api/courses`);
 
   const list = data?.courses.map((c) => <CourseRow course={c} key={c.id} />);
 
@@ -28,6 +117,9 @@ const Courses = () => {
     <Layout>
       <div className={styles.main}>
         <h1 className={styles.head}>All Courses</h1>
+        <div className={styles.options}>
+          <AddCourse />
+        </div>
         <div className={styles.list}>
           <div className={`${styles.course} ${styles.topCourse}`}>
             <span className={styles.idSpan}>ID</span>
